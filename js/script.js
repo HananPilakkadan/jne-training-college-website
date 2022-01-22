fbeaf796755c415a1e8940d512284c4c24803923
hananpilakkadan <hananpilakkadan@gmail.com>
2022-01-22 23:39:32 +0530

    uploaded

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(".owl-carousel").owlCarousel({
+  items: 1,
+  loop: true,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  autoplayTimeout: 5000,
+  dots: false,
+  touchDrag: false,
+  mouseDrag: false,
+  smartSpeed: 500,
+  animateIn: "fadeInUp",
+  animateOut: "fadeOut",
+});
