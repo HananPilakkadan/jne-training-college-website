7d7ccf23d5a4cc7e1c915b3b8e0a0208d43d4c35
hananpilakkadan <hananpilakkadan@gmail.com>
2022-01-23 16:58:32 +0530

    add-more-sections

--- js/script.js
+++ js/script.js
@@ -11,3 +11,53 @@ $(".owl-carousel").owlCarousel({
   animateIn: "fadeInUp",
   animateOut: "fadeOut",
 });
+// animation-revealing
+new WOW().init();
+
+// scrolling-animation
+AOS.init();
+
+// sticky-header
+window.onscroll = function () {
+  headerFunction();
+};
+
+var body = document.body;
+var sticky = body.offsetTop;
+
+function headerFunction() {
+  if (window.pageYOffset > 150) {
+    body.classList.add("sticky");
+  } else {
+    body.classList.remove("sticky");
+  }
+}
+
+function MobileMenu() {
+  let menuIcon = document.querySelector(".hamburger");
+  let body = document.querySelector("body");
+  let overlay = document.querySelector(".overlay");
+  menuIcon.addEventListener("click", function () {
+    body.classList.toggle("active");
+  });
+  overlay.addEventListener("click", function () {
+    body.classList.remove("active");
+  });
+  $(".mobile-container .mobile-menu li").click(() => {
+    body.classList.remove("active");
+  });
+}
+MobileMenu();
+
+//   activating-main-header-elements
+$("header .nav-bar li a").click(function () {
+  console.log("helooo");
+  $("header .wrapper .nav-bar li a.active").removeClass("active");
+  $(this).addClass("active");
+});
+
+//   activating-elements-mobile-menu
+$(".mobile-container .mobile-menu li").click(function () {
+  $(".mobile-container .mobile-menu li.active").removeClass("active");
+  $(this).addClass("active");
+});
